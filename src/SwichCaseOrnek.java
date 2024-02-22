d93d7dd1f884d0f7358e586ebbe3f7d485c765c6
Burçak ÖZTÜRK <159647466+BurcakOzturk@users.noreply.github.com>
2024-02-22 19:12:32 +0300

    switchcaseornek

---
+++ src/SwichCaseOrnek.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+public class SwichCaseOrnek {
+
+	public static void main(String[] args) {
+	
+		int ay ;
+		
+		Scanner input= new Scanner (System.in);
+		
+		ay=input.nextInt();
+	    switch (ay) {
+		
+	
+		
+		case 1:
+			System.out.println("ocak");
+			break;
+			
+			
+		case 2:
+			System.out.println("subat");
+			break;
+			
+		case 3:
+			System.out.println("mart");
+			break;
+			
+		case 4:
+			System.out.println("nisan");
+			break;
+			
+		case 5:
+			System.out.println("mayıs");
+			break;
+			
+			
+		case 6:
+			System.out.println("haziran");
+			break;
+			
+		case 7:
+			System.out.println("tenmuz");
+			break;
+			
+			
+		case 8:
+			System.out.println("ağustos");
+			break;
+			
+		case 9:
+			System.out.println("eylül");
+			break;
+			
+		case 10:
+			System.out.println("ekim");
+			break;
+			
+		case 11:
+			System.out.println("kasım");
+			break;
+			
+			
+		case 12:
+			System.out.println("aralık");
+			break;
+
+		}
+		
+		
+	}
+
+}
